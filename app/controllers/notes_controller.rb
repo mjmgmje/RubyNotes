90c07cfbf47850b60f998cb429a5bf295b594b0e
mjmgmje <mbellidolama@gmail.com>
2020-06-11 13:32:24 +0200

    added CRUD for notes and alerts bootstrap

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,6 @@
 class NotesController < ApplicationController
   def index
-    @notes = Note.where("user_id = #{session[:user_id]}")    
+    @notes = Note.where("user_id = #{session[:user_id]}") 
   end
 
   def show
@@ -16,12 +16,18 @@ class NotesController < ApplicationController
     requestNote[:user_id]=session[:user_id]
     @note = Note.create(requestNote)
     @note[:user_id] = session[:user_id]
-    redirect_to '/welcome'
+    redirect_to '/notes/index'
   end
 
-  def delete
+  def destroy
     @note = Note.find(params[:id])  
     @note.destroy
+
+    respond_to do |format|
+      format.html { redirect_to index, notice: 'Note was deleted succesfully'}
+      format.json { head :no_content }
+      format.js   { render :layout => false }
+    end
   end
   
   def update
@@ -30,5 +36,42 @@ class NotesController < ApplicationController
     noteOld.update(noteNew)
     redirect_to '/welcome'
   end
+
+  def noteChange
+    @note = Note.find(params[:id])  
+    puts("notechange run")
+    
+    respond_to do |format|
+      format.html { redirect_to index, notice: 'Note ready to change'}
+      format.json { head :no_content }
+      format.js   { render :layout => false }
+    end
+  end
+
+  def noteMod
+    noteOld = Note.find(params[:id])
+
+    respond_to do |format|
+      format.html { redirect_to index, notice: 'Note ready to change'}
+      format.json { head :no_content }
+      format.js   { render :layout => false }
+    end
+  end
+
+  def update
+    noteOld = Note.find(params[:id])
+    noteNew = {}
+    puts("update run");
+    noteNew[:note_text] = params[:note_text]
+    noteOld.update(noteNew)
+    
+    respond_to do |format|
+      format.html { redirect_to index, notice: 'Note ready to change'}
+      format.json { head :no_content }
+      format.js   { render :layout => false}
+    end
+    # redirect_to '/user/index'
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   get '', to: 'sessions#create'
   delete 'user/:id', to: 'users#destroy', :as => :user
+  delete 'note/:id', to: 'notes#destroy', :as => :note
   get 'user/index', to: 'users#index'
   get 'user/newAdmin', to: 'users#newAdmin'
   post 'user/newAdmin', to: 'users#createAdmin'
@@ -18,7 +19,10 @@ Rails.application.routes.draw do
   put 'notes', to: 'notes#update'
   delete 'notes', to: 'notes#delete'
   get 'user/:id', to: 'users#updateView'
+  get 'note/:id', to: 'notes#noteChange'
   put 'user/:id', to: 'users#update'
+  # put 'noteMod/:id', to: 'notes#update'
+  put 'note/:id', to: 'notes#update'
 
  
 
